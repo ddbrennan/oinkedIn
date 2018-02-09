4389097f2857085ffdab5dba0f09deaf5b07e3f5
ddbrennan <ddbren@gmail.com>
2018-02-09 12:50:41 -0500

    meh

--- oinkedIn-frontend/src/constants/index.js
+++ oinkedIn-frontend/src/constants/index.js
@@ -1,5 +1,5 @@
-export const API_ROOT = 'http://localhost:3001';
-export const API_WS_ROOT = 'ws://localhost:3001/cable';
+export const API_ROOT = 'http://0ed5afa1.ngrok.io';
+export const API_WS_ROOT = 'ws://0ed5afa1.ngrok.io/cable';
 export const HEADERS = {
   'Content-Type': 'application/json',
   Accept: 'application/json',
