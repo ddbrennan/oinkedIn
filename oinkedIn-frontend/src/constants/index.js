60684a3e6e2ef1cc9b2128091e3bb769abac34ce
ddbrennan <ddbren@gmail.com>
2018-02-14 14:40:04 -0500

    preparing for matt

--- oinkedIn-frontend/src/constants/index.js
+++ oinkedIn-frontend/src/constants/index.js
@@ -1,5 +1,5 @@
-export const API_ROOT = 'http://localhost:3001';
-export const API_WS_ROOT = 'ws://localhost:3001/cable';
+export const API_ROOT = 'http://94dcd25b.ngrok.io';
+export const API_WS_ROOT = 'ws://94dcd25b.ngrok.io/cable';
 
 export const HEADERS = {
   'Content-Type': 'application/json',
